a839e69ef378390e681aea3c7003720dd0bea47c
kosiyyu <kosiyyu@gmail.com>
2023-05-01 01:43:04 +0200

    add missing part

--- test/src/main/java/org/example/Car.java
+++ test/src/main/java/org/example/Car.java
@@ -1,2 +1,41 @@
-package org.example;public class Car {
+package org.example;
+
+public class Car {
+    private final String make;
+    private final String model;
+    private final int year;
+    private int mileage;
+
+    public Car(String make, String model, int year, int mileage) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.mileage = mileage;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void drive(int miles) {
+        mileage += miles;
+    }
+
+    @Override
+    public String toString() {
+        return year + " " + make + " " + model + ", " + mileage + " miles";
+    }
 }
+
--- test/src/test/java/org/example/CarTest.java
+++ test/src/test/java/org/example/CarTest.java
@@ -1,4 +1,41 @@
 package org.example;
 
-public class MainTest {
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CarTest {
+    @Test
+    public void testConstructor() {
+        String make = "Toyota";
+        String model = "Corolla";
+        int year = 2021;
+        int mileage = 0;
+        Car car = new Car(make, model, year, mileage);
+        assertEquals(make, car.getMake());
+        assertEquals(model, car.getModel());
+        assertEquals(year, car.getYear());
+        assertEquals(mileage, car.getMileage());
+    }
+
+    @Test
+    public void testDrive() {
+        int initialMileage = 10000;
+        int milesDriven = 500;
+        Car car = new Car("Honda", "Civic", 2018, initialMileage);
+        car.drive(milesDriven);
+        assertEquals(initialMileage + milesDriven, car.getMileage());
+    }
+
+    @Test
+    public void testToString() {
+        String make = "Toyota";
+        String model = "Corolla";
+        int year = 2021;
+        int mileage = 10000;
+        Car car = new Car(make, model, year, mileage);
+        String expectedString = year + " " + make + " " + model + ", " + mileage + " miles";
+        String actualString = car.toString();
+        assertEquals(expectedString, actualString);
+    }
 }
